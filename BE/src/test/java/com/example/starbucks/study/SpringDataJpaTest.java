4085b462d87e861763ddd946652dc25c3a24019e
Louie <dhdustnr0134@naver.com>
2022-05-13 16:54:26 +0900

    Feat: 이벤트 시작 날짜 내림차순 조회 기능 구현
    - Sort 객체 생성 관련 학습 테스트 추가

---
+++ BE/src/test/java/com/example/starbucks/study/SpringDataJpaTest.java
@@ -0,0 +1,30 @@
+package com.example.starbucks.study;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.example.starbucks.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mapping.PropertyReferenceException;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class SpringDataJpaTest {
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@Test
+	void Sort_객체를_생성할_때_엔티티의_필드명을_넣어줘야_한다() {
+		productRepository.findAll(Sort.by("koreanTitle"));
+	}
+
+	@Test
+	void Sort_객체를_생성할_때_데이터베이스_필드명을_넣어준다면_에러가_발생한다() {
+		assertThatThrownBy(() -> productRepository.findAll(Sort.by("korean_title")))
+			.isInstanceOf(PropertyReferenceException.class);
+	}
+}
